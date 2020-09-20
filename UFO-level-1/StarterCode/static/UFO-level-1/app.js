ebee822de265c0926b444067c2f783a67bd1dcbd
Chioma Maduko <cbmaduko@gmail.com>
2020-09-19 23:15:22 -0500

    submission files

---
+++ UFO-level-1/StarterCode/static/UFO-level-1/app.js
@@ -0,0 +1,48 @@
+  // from data.js
+  var tableData = data;
+
+  var tbody = d3.select("tbody");
+
+  function appendRowsAndData(obj) {
+      var row = tbody.append("tr");
+
+      // Below loop assumes object keys are in same order and are present every time
+      Object.entries(obj).forEach(([key, value]) => {
+          row.append("td").text(value); 
+      })
+  };
+
+  // Append all table rows and data
+  data.forEach(appendRowsAndData);
+
+  // Filter button
+  var button = d3.select("#filter-btn");
+
+  // This function only runs when the button is clicked
+  button.on("click", function() {
+      d3.event.preventDefault();
+
+      // Filter by date
+      var dateInput = d3.select("#datetime");
+      var datetime = dateInput.property("value");
+
+      var filterInputs = {};
+
+      if (datetime !== "") {
+          filterInputs.datetime = datetime;
+      }
+
+      var filtered = tableData.filter(obj => {
+          var criteria = true;
+          Object.entries(filterInputs).forEach(([key, value]) => {
+              criteria = criteria && (obj[key] === value);
+          });
+          return criteria;
+      });
+
+      console.log(filtered);
+
+      tbody.html("");
+
+      filtered.forEach(appendRowsAndData);
+  });
